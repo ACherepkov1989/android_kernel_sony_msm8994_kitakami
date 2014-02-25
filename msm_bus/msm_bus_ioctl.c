12858dc8ee9f15ddb6e079fedfb00c97d29d0932
Dan Sneddon <dsneddon@codeaurora.org>
2014-02-25 15:55:44 -0700

    msm_bus_test: Fix test after files moved.
    The bus scaling drivers moved from the platform specific directory
    to the platform drivers area.  This patch fixes the test to point
    to the new location of the bus scaling header files.
    
    Change-Id: I8d16e972951b7719bd75d932fd2cf104ed62aa3f

--- msm_bus/msm_bus_ioctl.c
+++ msm_bus/msm_bus_ioctl.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012, The Linux Foundation. All rights reserved.
+/* Copyright (c)2012,2014, The Linux Foundation. All rights reserved.
 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -16,8 +16,8 @@
 #include <linux/fs.h>
 #include <linux/miscdevice.h>
 #include <linux/uaccess.h>
-#include <mach/msm_bus.h>
-#include <mach/msm_bus_board.h>
+#include <linux/msm-bus.h>
+#include <linux/msm-bus-board.h>
 #include "msm_bus_test.h"
 
 static int msm_bus_create_client(unsigned long arg)
