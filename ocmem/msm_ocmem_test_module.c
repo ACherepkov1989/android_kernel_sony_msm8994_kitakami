e15e1f3d86dd86d7fe9ba8f1da5ecb1b9c1a43f6
Neeti Desai <neetid@codeaurora.org>
2014-02-26 17:56:00 -0800

    ocmem: change kernel header file includes
    The kernel header files for ocmem are no longer in
    arch/arm/mach-msm/include/mach, so the #includes
    need to be updated.
    
    Change-Id: If33fcda61f36237784e62ca205a69f9952ac2e07

--- ocmem/msm_ocmem_test_module.c
+++ ocmem/msm_ocmem_test_module.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -23,8 +23,7 @@
 #include <linux/io.h>
 #include <linux/miscdevice.h>
 #include <linux/uaccess.h>
-#include <mach/irqs.h>
-#include <mach/ocmem.h>
+#include <soc/qcom/ocmem.h>
 
 
 #define OCMEM_KERNEL_TEST_MAGIC 0xc1
