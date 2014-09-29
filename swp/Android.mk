7d682f81d74397cd04fed981d071c6b8da633131
Xiaocheng Li <lix@codeaurora.org>
2014-09-29 20:48:51 +0530

    swp: Build swp_test as 32bit binary on ARM64
    The swp_test module is to test if the kernel swp simulator is
    supported on the targets after ARMv5. However, the assembly
    instruction is not supported by AARCH64 compiler, so build this
    test module as 32bit binary on both ARM/ARM64 targets.
    
    Change-Id: I93978eed1e119113c1ce3bfd5eff3ede62b7afbb

--- swp/Android.mk
+++ swp/Android.mk
@@ -1,18 +1,26 @@
-ifeq ($(TARGET_ARCH),arm)
-ifeq (,$(filter arm64 aarch64, $(TARGET_KERNEL_ARCH)))
+ifneq (,$(filter arm arm64 aarch64, $(TARGET_ARCH)))
 
 LOCAL_PATH := $(call my-dir)
 commonSources :=
 
 include $(CLEAR_VARS)
-
 LOCAL_ARM_MODE := arm
 LOCAL_MODULE := swp_test
+LOCAL_MODULE_STEM_32 := swp_test
 LOCAL_SRC_FILES += $(commonSources) swp_test.c
 LOCAL_MODULE_TAGS := optional debug
 LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/kernel-tests
 LOCAL_MODULE_OWNER := qti
+LOCAL_MULTILIB := 32
 include $(BUILD_EXECUTABLE)
 
-endif
+# the test script
+include $(CLEAR_VARS)
+LOCAL_MODULE := swp_test.sh
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES := swp_test.sh
+LOCAL_MODULE_TAGS := optional debug
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/kernel-tests
+include $(BUILD_PREBUILT)
+
 endif
