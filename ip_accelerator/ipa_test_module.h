7e1cd2a0962b93dd738fdbc2e84c343a60a5b928
Gidon Studinski <gidons@codeaurora.org>
2015-03-03 10:13:50 +0200

    ip_accelerator: add generic configuration test framework
    Add the ability to configure IPA pipes from the user-space test
    application.
    
    Before this change, the configuration information was saved in
    the IPA kernel test-module and the user-space test application sent an
    index of that configuration.
    
    With this new framework the user-space test application asks for a
    specific configuration with all the required endpoint parameters.
    
    This allows the test logic to be kept in the user-space test application.
    The communication interface is based on a new IOCTL that is exposed from
    the IPA test-module to its user-space test component.
    
    Change-Id: I6b68d84be97797f1d0df33f8ea1ef1ee458102cd

--- ip_accelerator/ipa_test_module.h
+++ ip_accelerator/ipa_test_module.h
@@ -1,12 +1,46 @@
 #ifndef _IPA_TEST_MODULE_H_
 #define _IPA_TEST_MODULE_H_
 
-#include "linux/msm_ipa.h"
+#include <stddef.h>
+#include <linux/msm_ipa.h>
 #include <linux/ioctl.h>
+#ifdef _KERNEL_
+#include <linux/ipa.h>
+#endif
 
 #define IPA_TEST_IOC_MAGIC 0xA5
 #define IPA_TEST_IOCTL_GET_HW_TYPE 1
+#define IPA_TEST_IOCTL_CONFIGURE 2
+#define IPA_TEST_IOCTL_CLEAN 3
+
 #define IPA_TEST_IOC_GET_HW_TYPE _IO(IPA_TEST_IOC_MAGIC, \
 		IPA_TEST_IOCTL_GET_HW_TYPE)
+#define IPA_TEST_IOC_CONFIGURE _IOWR(IPA_TEST_IOC_MAGIC, \
+		IPA_TEST_IOCTL_CONFIGURE, \
+		struct ipa_test_config_header *)
+#define IPA_TEST_IOC_CLEAN _IO(IPA_TEST_IOC_MAGIC, \
+		IPA_TEST_IOCTL_CLEAN)
 
+#define IPA_TEST_CONFIG_MARKER 0x57
+#define IPA_TEST_CHANNEL_CONFIG_MARKER 0x83
+
+struct ipa_test_config_header
+{
+	char head_marker; /* IPA_TEST_CONFIG_MARKER */
+	int to_ipa_channels_num;
+	struct ipa_channel_config **to_ipa_channel_config;
+	int from_ipa_channels_num;
+	struct ipa_channel_config **from_ipa_channel_config;
+	char tail_marker; /* IPA_TEST_CONFIG_MARKER */
+};
+
+struct ipa_channel_config
+{
+	char head_marker; /* IPA_TEST_CHANNEL_CONFIG_MARKER */
+	enum ipa_client_type client;
+	int index; /* shall be used for to_ipa_x or from_ipa_x */
+	size_t config_size;
+	void *cfg;
+	char tail_marker; /* IPA_TEST_CHANNEL_CONFIG_MARKER */
+};
 #endif /* _IPA_TEST_MODULE_H_ */
