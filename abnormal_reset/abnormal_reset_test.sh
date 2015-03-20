6bb0ff4ad6fb3ee5669d47dc41d078c1b60f1e5e
Xiaogang Cui <xiaogang@codeaurora.org>
2015-03-20 14:36:32 +0800

    kernel-tests: add abnormal reset kernel tests for 8952
    Add Abnormal reset tests for msm8952.
    
    Change-Id: Ibdae5e7f7e89797c637cc873cad86497edf77354

--- abnormal_reset/abnormal_reset_test.sh
+++ abnormal_reset/abnormal_reset_test.sh
@@ -1,6 +1,6 @@
 #! /bin/sh --
 
-# Copyright (c) 2014, The Linux Foundation. All rights reserved.
+# Copyright (c) 2014-2015, The Linux Foundation. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
@@ -44,7 +44,7 @@ fi
 tsensor_reset_mod=${modpath}/tsensor_reset_mod.ko
 
 case "$TARGET_TYPE" in
-	"8916" | "8939" | "8909")
+	"8916" | "8939" | "8909" | "8952")
 		SPMI_PON_BASE=0x800;
 		SID=0x0;
 	;;
