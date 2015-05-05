99df805e6669dac6827207bff6c1fbc00277ee03
Thomas Gleixner <tglx@linutronix.de>
2015-05-05 19:49:49 +0200

    sched: Handle priority boosted tasks proper in setscheduler()
    [ Upstream commit 0782e63bc6fe7e2d3408d250df11d388b7799c6b ]
    
    Ronny reported that the following scenario is not handled correctly:
    
    	T1 (prio = 10)
    	   lock(rtmutex);
    
    	T2 (prio = 20)
    	   lock(rtmutex)
    	      boost T1
    
    	T1 (prio = 20)
    	   sys_set_scheduler(prio = 30)
    	   T1 prio = 30
    	   ....
    	   sys_set_scheduler(prio = 10)
    	   T1 prio = 30
    
    The last step is wrong as T1 should now be back at prio 20.
    
    Commit c365c292d059 ("sched: Consider pi boosting in setscheduler()")
    only handles the case where a boosted tasks tries to lower its
    priority.
    
    Fix it by taking the new effective priority into account for the
    decision whether a change of the priority is required.
    
    Reported-by: Ronny Meeus <ronny.meeus@gmail.com>
    Tested-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Steven Rostedt <rostedt@goodmis.org>
    Cc: <stable@vger.kernel.org>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: H. Peter Anvin <hpa@zytor.com>
    Cc: Mike Galbraith <umgwanakikbuti@gmail.com>
    Fixes: c365c292d059 ("sched: Consider pi boosting in setscheduler()")
    Link: http://lkml.kernel.org/r/alpine.DEB.2.11.1505051806060.4225@nanos
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>
    Signed-off-by: Andrey Cherepkov <andreiskull@gmail.com>

--- include/linux/sched/rt.h
+++ include/linux/sched/rt.h
@@ -18,7 +18,7 @@ static inline int rt_task(struct task_struct *p)
 #ifdef CONFIG_RT_MUTEXES
 extern int rt_mutex_getprio(struct task_struct *p);
 extern void rt_mutex_setprio(struct task_struct *p, int prio);
-extern int rt_mutex_check_prio(struct task_struct *task, int newprio);
+extern int rt_mutex_get_effective_prio(struct task_struct *task, int newprio);
 extern struct task_struct *rt_mutex_get_top_task(struct task_struct *task);
 extern void rt_mutex_adjust_pi(struct task_struct *p);
 static inline bool tsk_is_pi_blocked(struct task_struct *tsk)
@@ -30,10 +30,10 @@ static inline int rt_mutex_getprio(struct task_struct *p)
 {
 	return p->normal_prio;
 }
-
-static inline int rt_mutex_check_prio(struct task_struct *task, int newprio)
+static inline int rt_mutex_get_effective_prio(struct task_struct *task,
+					      int newprio)
 {
-	return 0;
+	return newprio;
 }
 static inline struct task_struct *rt_mutex_get_top_task(struct task_struct *task)
 {
@@ -47,7 +47,6 @@ static inline bool tsk_is_pi_blocked(struct task_struct *tsk)
 #endif
 
 extern void normalize_rt_tasks(void);
-
 
 /*
  * default timeslice is 100 msecs (used only for SCHED_RR tasks).
