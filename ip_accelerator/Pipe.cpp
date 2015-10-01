a0fa2a45c90a628c4e15f24f5b69d99da197f582
Skylar Chang <chiaweic@codeaurora.org>
2015-10-01 15:40:54 -0700

    kernel-tests: ip_accelerator: use GSI as transport layer
    This change adds the support for standalone tests to connect
    to IPA via GSI
    
    Change-Id: I049e11735ea8b636eda91942ff407dfd14faf467

--- ip_accelerator/Pipe.cpp
+++ ip_accelerator/Pipe.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2014-2015, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -62,7 +62,7 @@ Pipe::~Pipe() {
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 bool Pipe::Init() {
-	int tries_cnt = 10;
+	int tries_cnt = 10000;
 	SetSpecificClientParameters(m_nClientType, m_eConfiguration);
 	//By examining the Client type we will map the inode device name
 	while (tries_cnt > 0) {
