f8369e81f5e1852d0d021feca852366b7c6cbbed
Angelo G. Del Regno <kholk11@gmail.com>
2016-09-27 12:29:03 +0200

    clk: msm: clock: Remove MSM8916 handoff_vdd unvoting hack
    This hack was making sure we didn't unvote the unneeded voltages
    that the bootloader did set on our HW blocks because we had a
    severe bug with our I2C regulator.
    
    The bug is solved and now we can unvote the voltages in the
    handoff vdd list.

--- drivers/clk/msm/clock.c
+++ drivers/clk/msm/clock.c
@@ -1394,13 +1394,13 @@ static int __init clock_late_init(void)
 		list_del(&h->list);
 		kfree(h);
 	}
-#ifndef CONFIG_ARCH_MSM8916 /* THIS IS AN IDIOT HACK */
+
 	list_for_each_entry_safe(v, v_temp, &handoff_vdd_list, list) {
 		unvote_vdd_level(v->vdd_class, v->vdd_class->num_levels - 1);
 		list_del(&v->list);
 		kfree(v);
 	}
-#endif
+
 	mutex_unlock(&msm_clock_init_lock);
 
 	return ret;
