8e72bf2473d9b1a965e93e8eea82065ab0364ad3
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-10-11 23:52:08 +0200

    mmc: enable Enhance Strobe HS400 flow
    patch is needed for the tone platform
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -1286,14 +1286,11 @@ static int mmc_select_timing(struct mmc_card *card)
 		goto bus_speed;
 
 	/* For Enhance Strobe HS400 flow */
-/*
 	if (card->ext_csd.strobe_support &&
 	    card->mmc_avail_type & EXT_CSD_CARD_TYPE_HS400 &&
 	    card->host->caps & MMC_CAP_8_BIT_DATA)
 		err = mmc_select_hs400(card);
-	else 
-*/
-	if (card->mmc_avail_type & EXT_CSD_CARD_TYPE_HS200)
+	else if (card->mmc_avail_type & EXT_CSD_CARD_TYPE_HS200)
 		err = mmc_select_hs200(card);
 	else if (card->mmc_avail_type & EXT_CSD_CARD_TYPE_HS)
 		err = mmc_select_hs(card);
