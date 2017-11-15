2e11eb7d78524a1d816a710e7c455658e113269a
GreyLeshy <andreiskull@gmail.com>
2017-11-15 23:05:37 +0300

    init.qcom.post_boot.sh: fix the parameters LMK

--- init.qcom.post_boot.sh
+++ init.qcom.post_boot.sh
@@ -46,16 +46,29 @@ function configure_memory_parameters() {
     MemTotalStr=`cat /proc/meminfo | grep MemTotal`
     MemTotal=${MemTotalStr:16:8}
 
-    echo 0 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
+    # Read adj series and set adj threshold for ALMK.
+    # This is required since adj values change from framework to framework.
+    adj_series=`cat /sys/module/lowmemorykiller/parameters/adj`
+    adj_1="${adj_series#*,}"
+    set_almk_ppr_adj="${adj_1%%,*}"
 
-    #if [ "$arch_type" == "aarch64" ] && [ $MemTotal -gt 2097152 ]; then
-    #    echo "18432,23040,27648,32256,55296,80640" > /sys/module/lowmemorykiller/parameters/minfree
-    #    echo 81250 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
-    #elif [ "$arch_type" == "aarch64" ] && [ $MemTotal -gt 1048576 ]; then
-    #    echo 1 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
-    #    echo "18432,23040,27648,32256,55296,80640" > /sys/module/lowmemorykiller/parameters/minfree
-    #    echo 81250 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
-    #fi
+    # ALMK should not act on HOME adj and below.
+    # Normalized ADJ for HOME is 6. Hence multiply by 6
+    # ADJ score represented as INT in LMK params, actual score can be in decimal
+    # Hence add 6 considering a worst case of 0.9 conversion to INT (0.9*6).
+    set_almk_ppr_adj=$(((set_almk_ppr_adj * 6) + 6))
+    echo $set_almk_ppr_adj > /sys/module/lowmemorykiller/parameters/adj_max_shift
+
+    # Enable Adaptive LMK
+    echo 1 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
+
+    if [ "$arch_type" == "aarch64" ] && [ $MemTotal -gt 2097152 ]; then
+        echo "18432,23040,27648,32256,55296,80640" > /sys/module/lowmemorykiller/parameters/minfree
+        echo 81250 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
+    elif [ "$arch_type" == "aarch64" ] && [ $MemTotal -gt 1048576 ]; then
+        echo "14746,18432,22118,25805,40000,55000" > /sys/module/lowmemorykiller/parameters/minfree
+        echo 81250 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
+    fi
 
     # Zram disk - 512MB size
     # Zram configuration is done in init.target.rc
