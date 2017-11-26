ad8673d86f8e780c87dfc6303796a18ea1df8dcc
GreyLeshy <andreiskull@gmail.com>
2017-11-26 20:33:39 +0300

    adjust stress table

--- common/service.sh
+++ common/service.sh
@@ -6,11 +6,11 @@ MODDIR=${0%/*}
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
 
-sleep 45
+sleep 5
 echo "725 745 765 795 825 845 865 945 975 995 1015 1025 1035 1045 745 755 755 825 845 865 895 905 915 925 940 955 965 975 985" > /sys/devices/system/cpu/cpu0/cpufreq/UV_mV_table
 
-sleep 120
-echo "725 745 765 795 825 845 865 945 965 985 995 1015 1025 1035 745 755 755 825 845 865 895 905 915 925 940 955 965 975 985" > /sys/devices/system/cpu/cpu0/cpufreq/UV_mV_table
+sleep 75
+echo "725 745 765 795 825 845 865 945 965 985 995 1005 1025 1035 735 735 735 805 835 865 895 905 915 925 940 955 965 975 985" > /sys/devices/system/cpu/cpu0/cpufreq/UV_mV_table
 
 sleep 360
-echo "725 745 765 795 825 845 865 945 965 985 995 1015 1025 1035 745 755 755 825 845 865 895 905 915 925 940 955 965 975 985" > /sys/devices/system/cpu/cpu0/cpufreq/UV_mV_table
+echo "725 745 765 795 825 845 865 945 965 985 995 1005 1025 1035 735 735 735 805 835 865 895 905 915 925 940 955 965 975 985" > /sys/devices/system/cpu/cpu0/cpufreq/UV_mV_table
